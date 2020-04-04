ca70d59037f83c5fbf83f53fc67681dd7ebe115c
William Quigley <quigley.william@gmail.com>
2020-04-04 19:12:44 +0100

    initial commit with project structure

---
+++ src/test/java/com/example/hilo/Tests.java
@@ -0,0 +1,13 @@
+package com.example.hilo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
